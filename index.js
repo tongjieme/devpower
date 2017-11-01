e3b592ce12e7497d1b79e5de6617e9a273212cee
tongjiewen <tongjiewen@gmail.com>
2017-11-01 17:08:27 +0800

    add es6

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ var path = require('path');
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var less = require('gulp-less');
+var babel = require('gulp-babel');
+var rename = require("gulp-rename");
+var gutil = require('gulp-util');
+var plumber = require('gulp-plumber');
 
 var CWD = process.cwd();
 
@@ -23,6 +27,20 @@ var build = {
         return gulp.src(path.resolve(CWD, '**/*.less'))
             .pipe(less().on('error', sass.logError))
             .pipe(gulp.dest(path.resolve(CWD, './')));
+    },
+    es6: function () {
+        return gulp.src(path.resolve(CWD, '**/*.es6'))
+            .pipe(plumber())
+            .pipe(babel({
+                presets: ["babel-preset-es2015", "babel-preset-es2016", "babel-preset-es2017"].map(require.resolve)
+            }))
+            .on('error', function (e) {
+                gutil.log(e);
+            })
+            .pipe(rename(function (path) {
+                path.extname = ".js"
+            }))
+            .pipe(gulp.dest(path.resolve(CWD, './')));
     }
 };
 
@@ -32,7 +50,10 @@ gulp.task('sass:watch', function () {
 gulp.task('less:watch', function () {
     gulp.watch(path.resolve(CWD, '**/*.less'), build.less);
 });
+gulp.task('es6:watch', function () {
+    gulp.watch(path.resolve(CWD, '**/*.es6'), build.es6);
+});
 
-gulp.start(["sass:watch", 'less:watch'], function () {
-    console.log('ready');
+gulp.start(["sass:watch", 'less:watch', 'es6:watch'], function () {
+    console.log('laziness is ready...');
 });
